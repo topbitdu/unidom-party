ea563e65e8fccc7ce1dfa7fc161a66c68b83025c
Topbit Du <topbit.du@gmail.com>
2016-10-04 01:25:54 +0800

    1, Improve the Collaborating model to add the .collaborate! method.

--- app/models/unidom/party/collaborating.rb
+++ app/models/unidom/party/collaborating.rb
@@ -14,4 +14,17 @@ class Unidom::Party::Collaborating < ActiveRecord::Base
   scope :collaboration_is, ->(collaboration) { where collaboration: collaboration }
   scope :collaborated_by,  ->(collaborator)  { where collaborator:  collaborator  }
 
+  def self.collaborate!(collaboration: nil, collaborator: nil, name: collaborator.try(:name), role_code: nil, priority: 1, grade: 1, description: nil, instruction: nil, opened_at: Time.now)
+    query         = collaboration_is(collaboration).collaborated_by(collaborator).role_coded_as(role_code).valid_at(now: opened_at).alive
+    attributes    = { name: name, priority: priority, grade: grade, description: description, instruction: instruction }
+    collaborating = query.first_or_initialize attributes
+    if collaborating.new_record?
+      collaborating.opened_at = opened_at
+    else
+      collaborating.attributes = attributes
+    end
+    collaborating.save!
+    collaborating
+  end
+
 end

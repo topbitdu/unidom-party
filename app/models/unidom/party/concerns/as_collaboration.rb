940b6d1fe9894a1bcec2484f2d40b9ce36c30575
Topbit Du <topbit.du@gmail.com>
2016-10-10 07:58:46 +0800

    1, Improve the As Collaboration concern to add the #is_collaborated! method.

--- app/models/unidom/party/concerns/as_collaboration.rb
+++ app/models/unidom/party/concerns/as_collaboration.rb
@@ -6,6 +6,28 @@ module Unidom::Party::Concerns::AsCollaboration
 
     has_many :collaboratings, class_name: 'Unidom::Party::Collaborating', as: :collaboration
 
+    def is_collaborated!(by: nil, as: nil, at: Time.now, name: nil, priority: 0, grade: 0)
+      collaborating = collaboratings.collaborated_by(by).role_coded_as(as).valid_at(now: at).alive.first
+      if collaborating.present?
+        collaborating.attributes = { name: name, priority: priority, grade: grade }
+        collaborating.save!
+      else
+        collaborating = collaboratings.create! collaborator: by, role_code: as, name: name, priority: priority, grade: grade, opened_at: at
+      end
+      collaborating
+    end
+
+=begin
+    def is_collaborated?(by: nil, as: nil, at: Time.now, priority: nil, grade: nil)
+      query = collaboratings
+      query = query.collaborated_by by       if by.present?
+      query = query.role_coded_as   as       if as.present?
+      query = query.priority_is     priority if priority.present?
+      query = query.grade_is        grade    if grade.present?
+      query.valid_at(now: at).alive.exists?
+    end
+=end
+
   end
 
   module ClassMethods

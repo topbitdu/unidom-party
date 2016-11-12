3b09b2c740a0086e58038ccc2e8e2ce8a7366801
Topbit Du <topbit.du@gmail.com>
2016-11-12 13:35:50 +0800

    1, Improve the engine to load the enum YAML configuration files.

--- lib/unidom/party/engine.rb
+++ lib/unidom/party/engine.rb
@@ -1,8 +1,14 @@
+require 'unidom/common/yaml_helper'
+
 module Unidom
   module Party
     class Engine < ::Rails::Engine
 
       isolate_namespace ::Unidom::Party
+
+      initializer :load_config_initializers do |app|
+        Unidom::Common::YamlHelper.load_enum config: app.config, root: config.root
+      end
 
       initializer :append_migrations do |app|
         config.paths['db/migrate'].expanded.each { |expanded_path| app.config.paths['db/migrate'] << expanded_path } unless app.root.to_s.match root.to_s

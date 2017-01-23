56d1fb2c2191786a902d3ef4a0af9f16cf7fe74f
Topbit Du <topbit.du@gmail.com>
2017-01-23 09:57:05 +0800

    1, Improve the Shop model to support the namespace neglecting.

--- app/models/unidom/party/shop.rb
+++ app/models/unidom/party/shop.rb
@@ -11,4 +11,4 @@ class Unidom::Party::Shop < Unidom::Party::ApplicationRecord
 
   validates :name, presence: true, length: { in: 2..self.columns_hash['name'].limit }
 
-end
+end unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Party::Shop'

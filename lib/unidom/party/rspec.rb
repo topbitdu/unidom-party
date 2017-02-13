a8071cbee2769c70b2603937f7d0d1b51acee1c8
Topbit Du <topbit.du@gmail.com>
2017-02-13 09:52:37 +0800

    1, Remove the RSpec manifest.

--- lib/unidom/party/rspec.rb
+++
@@ -1,4 +0,0 @@
-require 'rspec/models/unidom/party/concerns/as_source_party_shared_examples'
-
-require 'rspec/models/unidom/party/party_relation_spec'
-require 'rspec/models/unidom/party/person_spec'

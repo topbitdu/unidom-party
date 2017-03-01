16486108dafa0748ca0e25b52b1cbd474ec5998c
Topbit Du <topbit.du@gmail.com>
2017-03-01 22:21:47 +0800

    1, Improve the Shop spec for the validations on the #name attribute.

--- lib/rspec/models/unidom/party/shop_spec.rb
+++ lib/rspec/models/unidom/party/shop_spec.rb
@@ -12,7 +12,26 @@ describe Unidom::Party::Shop, type: :model do
       name: 'WalMart'
     }
 
+    name_max_length = described_class.columns_hash['name'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :name,
+      {             } => 0,
+      { name: nil   } => 2,
+      { name: ''    } => 2,
+      { name: '1'   } => 1,
+      { name: 'A'   } => 1,
+      { name: '11'  } => 0,
+      { name: 'AA'  } => 0,
+      { name: '111' } => 0,
+      { name: 'AAA' } => 0,
+      { name: '1'*(name_max_length-1) } => 0,
+      { name: 'A'*(name_max_length-1) } => 0,
+      { name: '1'*name_max_length     } => 0,
+      { name: 'A'*name_max_length     } => 0,
+      { name: '1'*(name_max_length+1) } => 1,
+      { name: 'A'*(name_max_length+1) } => 1
 
   end
 

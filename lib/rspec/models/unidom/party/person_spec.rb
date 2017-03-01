45f47ff3ef96e94085a8cc40f6f8ab6105f8f6ca
Topbit Du <topbit.du@gmail.com>
2017-03-01 22:20:31 +0800

    1, Improve the Person spec for the validations on the #name attribute.

--- lib/rspec/models/unidom/party/person_spec.rb
+++ lib/rspec/models/unidom/party/person_spec.rb
@@ -6,35 +6,33 @@ describe Unidom::Party::Person, type: :model do
   after :each do
   end
 
-  context '.validates' do
-
-    model_attributes = { name: 'Tim' }
-
-    it_behaves_like 'validates', model_attributes, :name,
-      {             } => 0,
-      { name: nil   } => 2,
-      { name: ''    } => 2,
-      { name: 'A'   } => 1,
-      { name: 'AA'  } => 0,
-      { name: 'AAA' } => 0,
-      { name: '0'   } => 1,
-      { name: '00'  } => 0,
-      { name: '000' } => 0,
-      { name: 0     } => 1,
-      { name: 'A'*(described_class.columns_hash['name'].limit-1) } => 0,
-      { name: 'A'*described_class.columns_hash['name'].limit     } => 0,
-      { name: 'A'*(described_class.columns_hash['name'].limit+1) } => 1
-
-  end
-
   context do
 
     model_attributes = {
       name: 'Tim'
     }
 
+    name_max_length = described_class.columns_hash['name'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
     it_behaves_like 'Unidom::Party::Concerns::AsSourceParty',   model_attributes
+
+    it_behaves_like 'validates', model_attributes, :name,
+      {             } => 0,
+      { name: nil   } => 2,
+      { name: ''    } => 2,
+      { name: '1'   } => 1,
+      { name: 'A'   } => 1,
+      { name: '11'  } => 0,
+      { name: 'AA'  } => 0,
+      { name: '111' } => 0,
+      { name: 'AAA' } => 0,
+      { name: '1'*(name_max_length-1) } => 0,
+      { name: 'A'*(name_max_length-1) } => 0,
+      { name: '1'*name_max_length     } => 0,
+      { name: 'A'*name_max_length     } => 0,
+      { name: '1'*(name_max_length+1) } => 1,
+      { name: 'A'*(name_max_length+1) } => 1
 
   end
 

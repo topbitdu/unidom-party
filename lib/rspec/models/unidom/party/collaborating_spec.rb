26c672ec31b55233bc74d6ded72d590182bb3812
Topbit Du <topbit.du@gmail.com>
2017-03-07 09:20:57 +0800

    1, Improve the Collaborating spec for the ``belongs_to :collaborator, polymorphic: true`` macro.

--- lib/rspec/models/unidom/party/collaborating_spec.rb
+++ lib/rspec/models/unidom/party/collaborating_spec.rb
@@ -40,6 +40,16 @@ describe Unidom::Party::Collaborating, type: :model do
       { name: '1'*(name_max_length+1) } => 1,
       { name: 'A'*(name_max_length+1) } => 1
 
+    company_attributes           = { name: 'Tesla'   }
+    government_agency_attributes = { name: 'CD Gov'  }
+    person_attributes            = { name: 'Tim'     }
+    shop_attributes              = { name: 'WalMart' }
+
+    it_behaves_like 'belongs_to', model_attributes, :collaborator, Unidom::Party::Company,          company_attributes
+    it_behaves_like 'belongs_to', model_attributes, :collaborator, Unidom::Party::GovernmentAgency, government_agency_attributes
+    it_behaves_like 'belongs_to', model_attributes, :collaborator, Unidom::Party::Person,           person_attributes
+    it_behaves_like 'belongs_to', model_attributes, :collaborator, Unidom::Party::Shop,             shop_attributes
+
   end
 
 end

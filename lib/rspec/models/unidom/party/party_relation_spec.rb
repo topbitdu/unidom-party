16388b69e127de2931c646c660a1b69bd4f99272
Topbit Du <topbit.du@gmail.com>
2017-03-08 09:34:47 +0800

    1, Improve the Party Relation spec for the source_party_is scope, & the target_party_is scope.

--- lib/rspec/models/unidom/party/party_relation_spec.rb
+++ lib/rspec/models/unidom/party/party_relation_spec.rb
@@ -26,6 +26,19 @@ describe Unidom::Party::PartyRelation, type: :model do
     it_behaves_like 'belongs_to', friendship_attributes.merge({ source_party_id: Unidom::Common::NULL_UUID, source_party_type: 'Unidom::Party::Person' }),
       :target_party, Unidom::Party::Person, tom_attributes
 
+    model_attributes = {
+      source_party_id:   SecureRandom.uuid,
+      source_party_type: 'Unidom::Party::Person',
+      target_party_id:   SecureRandom.uuid,
+      target_party_type: 'Unidom::Party::Person',
+      linkage_code:      'FRND',
+      priority:          1,
+      grade:             5
+    }
+
+    it_behaves_like 'polymorphic scope', model_attributes, :source_party_is, :source_party, [ Unidom::Party::Company, Unidom::Party::GovernmentAgency, Unidom::Party::Person, Unidom::Party::Shop ]
+    it_behaves_like 'polymorphic scope', model_attributes, :target_party_is, :target_party, [ Unidom::Party::Company, Unidom::Party::GovernmentAgency, Unidom::Party::Person, Unidom::Party::Shop ]
+
   end
 
 end

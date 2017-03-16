e209d8c8f9aa0715f058cd6df4c2e52213b7a06f
Topbit Du <topbit.du@gmail.com>
2017-03-16 13:19:08 +0800

    1, Improve the Person spec for the validations on the #name attribute.

--- lib/rspec/models/unidom/party/person_spec.rb
+++ lib/rspec/models/unidom/party/person_spec.rb
@@ -12,27 +12,10 @@ describe Unidom::Party::Person, type: :model do
       name: 'Tim'
     }
 
-    name_max_length = described_class.columns_hash['name'].limit
-
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
     it_behaves_like 'Unidom::Party::Concerns::AsSourceParty',   model_attributes
 
-    it_behaves_like 'validates', model_attributes, :name,
-      {             } => 0,
-      { name: nil   } => 2,
-      { name: ''    } => 2,
-      { name: '1'   } => 1,
-      { name: 'A'   } => 1,
-      { name: '11'  } => 0,
-      { name: 'AA'  } => 0,
-      { name: '111' } => 0,
-      { name: 'AAA' } => 0,
-      { name: '1'*(name_max_length-1) } => 0,
-      { name: 'A'*(name_max_length-1) } => 0,
-      { name: '1'*name_max_length     } => 0,
-      { name: 'A'*name_max_length     } => 0,
-      { name: '1'*(name_max_length+1) } => 1,
-      { name: 'A'*(name_max_length+1) } => 1
+    it_behaves_like 'validates text', model_attributes, :name, length: 2..described_class.columns_hash['name'].limit
 
   end
 

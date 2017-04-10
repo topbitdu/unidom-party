b653c27484d804de9fb22edc4cfc618dae1479b5
Topbit Du <topbit.du@gmail.com>
2017-04-10 09:43:33 +0800

    1, Improve the As Source Party concern for the argument validations of the #relate! method, & the #relate? method.

--- app/models/unidom/party/concerns/as_source_party.rb
+++ app/models/unidom/party/concerns/as_source_party.rb
@@ -1,17 +1,30 @@
 module Unidom::Party::Concerns::AsSourceParty
 
-  extend ActiveSupport::Concern
+  extend  ActiveSupport::Concern
+  include Unidom::Common::Concerns::ArgumentValidation
 
   included do |includer|
 
     has_many :target_party_relations, class_name: 'Unidom::Party::PartyRelation', as: :source_party
 
     def relate!(to: nil, due_to: nil, at: Time.now)
+
+      assert_present! :to,     to
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+
       target_party_relations.create! target_party: to, linkage_code: due_to, opened_at: at
+
     end
 
     def relate?(to: nil, due_to: nil, at: Time.now)
+
+      assert_present! :to,     to
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+
       target_party_relations.target_party_is(to).linkage_coded_as(due_to).valid_at(now: at).alive.exists?
+
     end
 
   end

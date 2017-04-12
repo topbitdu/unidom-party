773953e65328b95e5588cbd99e5f76d5700382cf
Topbit Du <topbit.du@gmail.com>
2017-04-12 10:05:34 +0800

    1, Improve the As Target Party concern for the argument validations of the #is_related! method, & the #is_related? method.

--- app/models/unidom/party/concerns/as_target_party.rb
+++ app/models/unidom/party/concerns/as_target_party.rb
@@ -7,11 +7,23 @@ module Unidom::Party::Concerns::AsTargetParty
     has_many :source_party_relations, class_name: 'Unidom::Party::PartyRelation', as: :target_party
 
     def is_related!(to: nil, due_to: nil, at: Time.now)
+
+      assert_present! :to,     to
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+
       source_party_relations.create! source_party: to, linkage_code: due_to, opened_at: at
+
     end
 
     def is_related?(to: nil, due_to: nil, at: Time.now)
+
+      assert_present! :to,     to
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+
       source_party_relations.source_party_is(to).linkage_coded_as(due_to).valid_at(now: at).alive.exists?
+
     end
 
   end
